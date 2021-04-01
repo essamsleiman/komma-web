aa5fdeaac5e911fbab96684e7cc5c05f4674fc9e
James Junaidi <james.junaidi@gmail.com>
2021-04-01 10:03:26 -0700

    added event schema in code only

---
+++ backend/models/event.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({
+  eventName: { type: String, required: true },
+  hostName: { type: String, required: true },
+  // createdAt: { type: Date, required: Date.now },
+});
+
+const event = mongoose.model("event", eventSchema);
+
+module.exports = event;
