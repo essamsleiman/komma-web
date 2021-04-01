f39492d699f1913d6d2a1a20f865471f7d13d401
Edward Chew <edwardchew8@gmail.com>
2021-04-01 16:00:47 -0700

    Availability page left bar

--- src/components/availability.js
+++ src/components/availability.js
@@ -5,12 +5,13 @@ import TopBar from "./availability/top_bar"
 import "./css/availability.css";
 
 function Availability() {
+  const [viewingGroup, setViewingGroup] = useState(false);
 
   return (
     <div>
         <div className="row no-gutters justify-content-center shadow-card top-margin">
             <div className="col-3">
-                <LeftBar />
+                <LeftBar viewingGroup={viewingGroup} setViewingGroup={setViewingGroup} />
             </div>
             <div className="col-9">
                 <div className="vertical-bar"></div>
