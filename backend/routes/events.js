255d8d7346a636a70a27e4e537902a42e1dfec8a
James Junaidi <james.junaidi@gmail.com>
2021-04-06 14:21:29 -0700

    comments on events.js for fields in event object

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -18,10 +18,30 @@ router.route("/add").post((req, res) => {
     const hostname = req.body.hostname;
     const link = req.body.link;
 
-    const newEvent = new EVent({
-        name,
-        hostname,
-        link,
+    const newEvent = new Event({
+        // future fields to be added
+        // uniqueID: {type: id, required: true },
+        // hostID: { type: id, required: true},
+        // hostName: { type: String, required: true },
+        // title: { type: String, required: true },
+        // description: { type: String, required: true },
+        // location: { type: String, required: true },
+      
+        // minTimeRange: { type: Date, required: true},
+        // maxTimeRange: { type: Date, required: true},
+      
+        // respondents: { type: [String], required: true},
+        // hostAvailability: { type: [Date, Date], required: true },
+        // Availabilities: { type: [[Date, Date]], required: true },
+        // finalMeetingTime: { type: Date, requried: true},
+        // sendInvite: {
+        //   daysSentAfter: { type: Number, required: true},
+        //   registeredSentAfter: { type: Number, requried: true},
+        // },
+        // notifyOnResponse: { type: Boolean, required: true},
+        // availabilityHidden: { type: Boolean, requried: true},
+        // meetingInviteLink: {type: String, required: true},
+        // googleMeetLink: { type: String, required: true},
     });
     newEvent
         .save()
