0052dd9a2b98a4e05c75da5222f4e63dd290fa51
Essam Sleiman <essamfsleiman@gmail.com>
2021-04-05 22:42:47 -0700

    set up react redux

--- src/components/home.js
+++ src/components/home.js
@@ -1,38 +1,45 @@
 import React, { useState, useEffect } from "react";
-
+import { connect } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import axios from "axios";
 import Cookies from "universal-cookie";
+import PropTypes from "prop-types";
+
+import { fetchUser } from "../../src/Redux/actions/userActions";
+import { fetchCalendar } from "../../src/Redux/actions/calendarActions";
+
 const cookies = new Cookies();
 
-function Home() {
+function Home(props) {
   function signin() {}
   console.log("COOKIE", document.cookie);
-  const [userToken, setUserToken] = useState();
   useEffect(() => {
     //   val = profileInfoState.userEventData;
-    fetch("http://localhost:5000/auth/success", {
-      method: "GET",
-      credentials: "include",
-    })
-      .then((response) => {
-        if (response.status === 200) return response.json();
-        throw new Error("failed to authenticate user");
-      })
-      .then((responseJson) => {
-        if (responseJson) {
-          console.log("RESPONSE In HOme: ", responseJson);
-          setUserToken(responseJson);
-        } else {
-          console.log("no response from server");
-        }
-      })
-      .catch((error) => {
-        console.log(`error: ${error}`);
-      });
-  }, []);
+    // fetch("http://localhost:5000/auth/success", {
+    //   method: "GET",
+    //   credentials: "include",
+    // })
+    //   .then((response) => {
+    //     if (response.status === 200) return response.json();
+    //     throw new Error("failed to authenticate user");
+    //   })
+    //   .then((responseJson) => {
+    //     if (responseJson) {
+    //       console.log("RESPONSE In HOme: ", responseJson);
+    //       setUserToken(responseJson);
+    //     } else {
+    //       console.log("no response from server");
+    //     }
+    //   })
+    //   .catch((error) => {
+    //     console.log(`error: ${error}`);
+    //   });
 
+    props.fetchUser();
+  }, []);
+  console.log("CALENDAR REDUX: ", props.calendar);
+  console.log("USER REDUX: ", props.user.accessToken);
   return (
     <div>
       <button
@@ -57,20 +64,22 @@ function Home() {
       <button
         onClick={() => {
           console.log("FRONTEND ERROR ESSAM");
-          console.log("userToken:", userToken);
-          axios
-            .get("http://localhost:5000/auth/events", {
-              headers: {
-                Authorization: `Bearer ${userToken.accessToken}`,
-              },
-            })
-            .then((response) => {
-              if (response) {
-                console.log(response);
-              } else {
-                console.log("no response from server");
-              }
-            });
+          // console.log("userToken:", userToken);
+
+          props.fetchCalendar(props.user.accessToken);
+          // axios
+          //   .get("http://localhost:5000/auth/events", {
+          //     headers: {
+          //       Authorization: `Bearer ${props.user.accessToken}`,
+          //     },
+          //   })
+          //   .then((response) => {
+          //     if (response) {
+          //       console.log(response);
+          //     } else {
+          //       console.log("no response from server");
+          //     }
+          //   });
           //window.open("http://localhost:5000/auth/events", "_self");
         }}
       >
@@ -80,4 +89,16 @@ function Home() {
   );
 }
 
-export default Home;
+Home.propTypes = {
+  fetchUser: PropTypes.func.isRequired,
+  user: PropTypes.array.isRequired,
+  fetchCalendar: PropTypes.func.isRequired,
+  calendar: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  calendar: state.calendar.calendar.data.events,
+  user: state.user.user,
+});
+
+export default connect(mapStateToProps, { fetchUser, fetchCalendar })(Home);
