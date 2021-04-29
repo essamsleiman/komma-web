a91a581aea7ee96a06636ed64f5128b98b6e6aac
Essam Sleiman <49323622+essamsleiman@users.noreply.github.com>
2021-04-29 11:55:50 -0700

    Update step_three.js

--- src/components/meeting_creation/step_three.js
+++ src/components/meeting_creation/step_three.js
@@ -6,7 +6,7 @@ import "../css/step_three.css";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { updateEvent } from "../../Redux/actions/eventActions";
-import { timeConversion } from "../utils.js";
+//import { timeConversion } from "../utils.js";
 function StepThree(props) {
   const meetingLengths = [
    
@@ -25,6 +25,37 @@ function StepThree(props) {
     },
   ];
 
+  function timeConversion(time) {
+  const timeTableConversion = {
+    "12am": "00:00",
+    "1am": "01:00",
+    "2am": "02:00",
+    "3am": "03:00",
+    "4am": "04:00",
+    "5am": "05:00",
+    "6am": "06:00",
+    "7am": "07:00",
+    "8am": "08:00",
+    "9am": "09:00",
+    "10am": "10:00",
+    "11pm": "11:00",
+    "12pm": "12:00",
+    "1pm": "13:00",
+    "2pm": "14:00",
+    "3pm": "15:00",
+    "4pm": "16:00",
+    "5pm": "17:00",
+    "6pm": "18:00",
+    "7pm": "19:00",
+    "8pm": "20:00",
+    "9pm": "21:00",
+    "10pm": "22:00",
+    "11pm": "23:00",
+  };
+
+  return timeTableConversion[time];
+}
+  
   const meetingTimes = [
     {
       label: "12am",
